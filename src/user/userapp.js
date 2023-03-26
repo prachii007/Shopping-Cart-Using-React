c00c6f30e51a45047e744321aaa8b1db0070cd13
Prachi I <prachi.indurkar007@gmail.com>
2023-03-26 10:53:57 +0530

    import and call mycart function in userapp.js file

--- src/user/userapp.js
+++ src/user/userapp.js
@@ -1,10 +1,12 @@
 import Myhome from "./home";
 import Login from "./login";
+import Mycart from "./cart";
 const userApp = () => {
     return (
         <>
             <Myhome />
             <Login/>
+            <Mycart/>
         </>
     )
 }
