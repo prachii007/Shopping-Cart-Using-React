4cf373f32da05512ae9fb49673ba243195c3bcb0
Prachi I <prachi.indurkar007@gmail.com>
2023-03-26 10:56:56 +0530

    import and call publicheader function in userapp.js file

--- src/user/userapp.js
+++ src/user/userapp.js
@@ -1,12 +1,14 @@
 import Myhome from "./home";
 import Login from "./login";
 import Mycart from "./cart";
+import Publicheader from "./publicheader";
 const userApp = () => {
     return (
         <>
             <Myhome />
             <Login/>
             <Mycart/>
+            <Publicheader/>
         </>
     )
 }
