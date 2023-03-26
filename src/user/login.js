7c3f1e21fb3a926f0816d5a1e7961c9debc04864
Prachi I <prachi.indurkar007@gmail.com>
2023-03-26 12:33:07 +0530

    design a card for email and password in login file

--- src/user/login.js
+++ src/user/login.js
@@ -1,8 +1,31 @@
 const Login = () => {
     return (
-        <h1>
-            Login
-        </h1>
+        <section className="container mt-5">
+            <div className="row">
+                <div className="col-lg-4"></div>
+                <div className="col-lg-4">
+                    <div className="card border-0 shadow-lg">
+                        <div className="card-header bg-primary text-white">
+                            <i className="fa fa-lock"></i> Login
+                        </div>
+                        <div className="card-body">
+                            <div className="mb-3">
+                                <label>Email ID</label>
+                                <input className="form-control" type="text"/>
+                            </div>
+                            <div className="mb-3">
+                                <label>Password</label>
+                                <input className="form-control" type="password"/>
+                            </div>
+                        </div>
+                        <div className="card-footer text-center">
+                            <button className="btn btn-danger"> Login <i className="fa fa-arrow-right"></i></button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-4"></div>
+            </div>
+        </section>
     )
 }
 export default Login;
