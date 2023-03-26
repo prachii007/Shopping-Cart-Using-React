cb4b467a1eca3f110bb1a3afa64c9c96d99d5c6a
Prachi I <prachi.indurkar007@gmail.com>
2023-03-26 11:33:11 +0530

    in userapp.js file, properly route the files that were imported using hashrouter and routes

--- src/user/userapp.js
+++ src/user/userapp.js
@@ -1,15 +1,18 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Myhome from "./home";
 import Login from "./login";
 import Mycart from "./cart";
 import Publicheader from "./publicheader";
 const UserApp = () => {
     return (
-        <>
+        <HashRouter>
             <Publicheader />
-            <Myhome />
-            <Login />
-            <Mycart />
-        </>
+            <Routes>
+                <Route exact path="/" element={<Myhome />} />
+                <Route exact path="/" element={<Login />} />
+                <Route exact path="/" element={<Mycart />} />
+            </Routes>
+        </HashRouter >
     )
 }
 export default UserApp;
