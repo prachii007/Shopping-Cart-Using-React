502cd2f45abf9ed3563f680d136e506576bd4bbd
Prachi I <prachi.indurkar007@gmail.com>
2023-04-01 09:25:27 +0530

    import use state in home.js

--- src/user/home.js
+++ src/user/home.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
 const Myhome = () => {
     return (
-        <>
+        <main>
             <section id="banner">React Shopping App</section>
-        </>
+            <section className="container mt-4">
+                <div className="row"></div>
+            </section>
+        </main>
     )
 }
 export default Myhome;
