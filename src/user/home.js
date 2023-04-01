2cd0bb0aaf5d299f760ced9c0950a6714ba686be
Prachi I <prachi.indurkar007@gmail.com>
2023-04-01 09:36:04 +0530

    map open the product array in home.js file

--- src/user/home.js
+++ src/user/home.js
@@ -3,20 +3,33 @@ const Myhome = () => {
     let [allproduct, updateProduct] = useState([]);
     const getProduct = () => {
         fetch("http://localhost:1234/product")
-        .then(response=>response.json())
-        .then(productArray=>{
-            updateProduct(productArray);
-        })
+            .then(response => response.json())
+            .then(productArray => {
+                updateProduct(productArray);
+            })
     }
-    useEffect(()=>{
+    useEffect(() => {
         getProduct();
-    },[1]);
-    
+    }, [1]);
+
     return (
         <main>
             <section id="banner">React Shopping App</section>
             <section className="container mt-4">
-                <div className="row"></div>
+                <div className="row">
+                    {
+                        allproduct.map((product, index) => {
+                            return (
+                                <div className="col-lg-3 mb-5" key={index}>
+                                    <div className="p-3">
+                                        <h2 className="text-primary">{product.name}</h2>
+                                        <img className="rounded shadow" src={product.photo} height="200" width="100%"/>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
             </section>
         </main>
     )
