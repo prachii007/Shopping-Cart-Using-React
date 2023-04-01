65963c7f14d786ed896356cd5d982742216f506c
Prachi I <prachi.indurkar007@gmail.com>
2023-04-01 10:54:20 +0530

    fetch cart array from api and map open its content in tabular format in cart.js

--- src/user/cart.js
+++ src/user/cart.js
@@ -1,6 +1,61 @@
+import { useState, useEffect } from "react";
 const Mycart = () => {
+    let [allcart, updateCart] = useState([]);
+    const getCart = () => {
+        fetch("http://localhost:1234/cart")
+            .then(response => response.json())
+            .then(productArray => {
+                updateCart(productArray);
+            })
+    }
+    useEffect(() => {
+        getCart();
+    }, [1]);
     return (
-        <h1>My Cart</h1>
+        <main>
+            <h1 className="display-6 mt-3">My Cart</h1>
+            <div className="row">
+                <div className="col-lg-5"></div>
+                <div className="col-lg-7">
+                    <table className="table table-bordered">
+                        <thead>
+                            <tr className="text-center">
+                                <th>Sr No</th>
+                                <th>Product Name</th>
+                                <th>Product Price</th>
+                                <th>Quantity</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        {
+                            allcart.map((cart, index) => {
+                                return (
+
+                                    <tbody>
+                                        <tr key={index}  className="text-center">
+                                            <td>{index + 1}</td>
+                                            <td className="text-start"><img src={cart.photo} height="50" width="50" className="rounded" /> {cart.name}</td>
+                                            <td>{cart.price}</td>
+                                            <td>{cart.qty}</td>
+                                            <td><button className="btn btn-danger">Remove From Cart</button></td>
+                                        </tr>
+                                    </tbody>
+                                )
+                            })
+                        }
+                    </table>
+                </div>
+            </div>
+        </main>
+
     )
 }
 export default Mycart;
+
+/* fetch data from cart api and display in cart page
+Note:
+cart page
+row
+col-5 = empty
+col-7 = display cart in table format
+*/
