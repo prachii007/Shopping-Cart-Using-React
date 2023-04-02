07b73bbc936c80f993f4ecf91146bfe6f407c117
Prachi I <prachi.indurkar007@gmail.com>
2023-04-02 10:39:21 +0530

    define function to delete items, and to capture customer details

--- src/user/cart.js
+++ src/user/cart.js
@@ -11,42 +11,145 @@ const Mycart = () => {
     useEffect(() => {
         getCart();
     }, [1]);
+    let total = 0;
+    const one = (pid, product, action) => {
+        if (action === "B") {
+            product["qty"] = product.qty - 1;
+        } else {
+            product["qty"] = product.qty + 1;
+        }
+        if (product.qty > 0) {
+            let url = "http://localhost:1234/cart/" + pid;
+            let postOption = {
+                headers: { 'Content-Type': 'application/json' },
+                method: 'PUT',
+                body: JSON.stringify(product)
+            }
+            fetch(url, postOption)
+                .then(response => response.json())
+                .then(serverRes => {
+                    getCart();
+                })
+        } else {
+            deleteCart(pid);
+        }
+    }
+    const deleteCart = (pid) => {
+        let url = "http://localhost:1234/cart/" + pid;
+        let postOption = { method: "DELETE" };
+        fetch(url, postOption)
+            .then(response => response.json())
+            .then(serverRes => {
+                getCart(); //reload the list
+            })
+    }
+    //order placing code starts here
+    let [fullname, pickName] = useState("");
+    let [mobile, pickMobile] = useState("");
+    let [email, pickEmail] = useState("");
+    let [address, pickAddress] = useState("");
+    const save = () => {
+        let url = "http://localhost:1234/order/";
+        let orderData = {
+            //property: variable//
+            customername: fullname,
+            mobile: mobile,
+            email: email,
+            address: address,
+            orderItem: allcart
+        };
+        let postOption = {
+            headers: { 'Content-Type': 'application/json' },
+            method: "POST",
+            body: JSON.stringify(orderData)
+        };
+        fetch(url, postOption)
+            .then(response => response.json())
+            .then(serverRes => {
+                alert("Order Received, Order ID:" + serverRes.id)
+            })
+    }
     return (
-        <main>
-            <h1 className="display-6 mt-3">My Cart</h1>
+        <section className="container mt-4">
             <div className="row">
-                <div className="col-lg-5"></div>
-                <div className="col-lg-7">
+                <div className="col-lg-4">
+                    <h3 className="display-6">Customer Details</h3>
+                    <div className="card border-0 shadow-lg mt-4">
+                        <div className="card-header bg-primary text-white">Customer Details</div>
+                        <div className="card-body">
+                            <div className="mb-3">
+                                <label>Customer Name</label>
+                                <input type="text" className="form-control" onChange={obj => pickName(obj.target.value)} />
+                            </div>
+                            <div className="mb-3">
+                                <label>Mobile No</label>
+                                <input type="tel" className="form-control" onChange={obj => pickMobile(obj.target.value)} />
+                            </div>
+                            <div className="mb-3">
+                                <label>Email ID</label>
+                                <input type="email" className="form-control" onChange={obj => pickEmail(obj.target.value)} />
+                            </div>
+                            <div className="mb-3">
+                                <label>Delivery Address</label>
+                                <textarea type="text" className="form-control" onChange={obj => pickAddress(obj.target.value)}></textarea>
+                            </div>
+                        </div>
+                        <div className="card-footer text-center">
+                            <button onClick={save} className="btn btn-warning btn-sm">Place Order</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-8">
+                    <h3 className="text-center text-primary">{allcart.length} Items in Cart</h3>
                     <table className="table table-bordered">
                         <thead>
-                            <tr className="text-center">
+                            <tr className="text-center bg-light text-primary">
                                 <th>Sr No</th>
                                 <th>Product Name</th>
                                 <th>Product Price</th>
                                 <th>Quantity</th>
+                                <th>Total</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
-                        {
-                            allcart.map((cart, index) => {
-                                return (
-
-                                    <tbody>
-                                        <tr key={index}  className="text-center">
-                                            <td>{index + 1}</td>
+                        <tbody>
+                            {
+                                allcart.map((cart, index) => {
+                                    total = total + cart.qty * cart.price;
+                                    return (
+                                        <tr key={index} className="text-center">
+                                            <td>{cart.id}</td>
                                             <td className="text-start"><img src={cart.photo} height="50" width="50" className="rounded" /> {cart.name}</td>
                                             <td>{cart.price}</td>
-                                            <td>{cart.qty}</td>
-                                            <td><button className="btn btn-danger">Remove From Cart</button></td>
+                                            <td>
+                                                <div className="input-group">
+                                                    <button onClick={one.bind(this, cart.id, cart, "A")} className="btn btn-info">+</button>
+                                                    <input value={cart.qty} type="text" className="form-control" />
+                                                    <button onClick={one.bind(this, cart.id, cart, "B")} className="btn btn-info">-</button>
+                                                </div>
+                                            </td>
+                                            <td>{cart.qty * cart.price}</td>
+                                            <td><button onClick={deleteCart.bind(this, cart.id)} className=" btn-sm btn btn-danger">Remove From Cart</button></td>
                                         </tr>
-                                    </tbody>
-                                )
-                            })
-                        }
+                                    )
+                                })
+                            }
+                            <tr>
+                                <td>SGST - {total * 9 / 100}</td>
+                                <td>CGST - {total * 9 / 100}</td>
+                                <td colSpan="2">GST Amount - {(total * 9 / 100) + (total * 9 / 100)}</td>
+                                <td colSpan="2" className="">Rs. {total} </td>
+                            </tr>
+                            <tr>
+                                <td className="text-center" colSpan="6">
+                                    {total + (total * 18 / 100)} : Total Amount to Pay
+                                </td>
+                            </tr>
+                        </tbody>
                     </table>
                 </div>
             </div>
-        </main>
+        </section>
 
     )
 }
