cee60c8ce671d5cb7852d122344a07562aa14429
Prachi I <prachi.indurkar007@gmail.com>
2023-04-08 09:30:18 +0530

    write login validation code in login file

--- src/user/login.js
+++ src/user/login.js
@@ -1,9 +1,21 @@
+import { useState } from "react";
 const Login = () => {
+    let [msg, updatemsg] = useState("Enter Your Login Details");
+    let [username, pickUsername] = useState("");
+    let [pass, pickPassword] = useState("");
+    const goLogin = () => {
+        if (username == "" || pass == "") {
+            updatemsg("Empty Email or Password")
+        } else {
+            updatemsg("Please wait Validating...")
+        }
+    }
     return (
         <section className="container mt-5">
             <div className="row">
                 <div className="col-lg-4"></div>
                 <div className="col-lg-4">
+                    <p className="text-center text-danger">{msg}</p>
                     <div className="card border-0 shadow-lg">
                         <div className="card-header bg-primary text-white">
                             <i className="fa fa-lock"></i> Login
@@ -11,15 +23,17 @@ const Login = () => {
                         <div className="card-body">
                             <div className="mb-3">
                                 <label>Email ID</label>
-                                <input className="form-control" type="text"/>
+                                <input className="form-control" type="text" 
+                                onChange={obj=>pickUsername(obj.target.value)}/>
                             </div>
                             <div className="mb-3">
                                 <label>Password</label>
-                                <input className="form-control" type="password"/>
+                                <input className="form-control" type="password"
+                                onChange={obj=>pickPassword(obj.target.value)} />
                             </div>
                         </div>
                         <div className="card-footer text-center">
-                            <button className="btn btn-danger"> Login <i className="fa fa-arrow-right"></i></button>
+                            <button onClick={goLogin} className="btn btn-danger"> Login <i className="fa fa-arrow-right"></i></button>
                         </div>
                     </div>
                 </div>
