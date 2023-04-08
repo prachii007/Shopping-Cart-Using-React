baa3fcff94ed1924cc649d9b4142878d98dea219
Prachi I <prachi.indurkar007@gmail.com>
2023-04-08 09:07:33 +0530

    write if and else condition in app.js file for user or admin module to open by default

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import UserApp from "./user/userapp";
 import AdminApp from "./admin/adminapp";
 function App() {
-  return (
-    <>
-      <AdminApp />
-    </>
-  );
+  if (localStorage.getItem("adminid") == null) { //variable adminid is created. Asked if it is equal to null?
+    return (< UserApp />);
+  } else {
+    return (< AdminApp />);
+  }
 }
 
 export default App;
