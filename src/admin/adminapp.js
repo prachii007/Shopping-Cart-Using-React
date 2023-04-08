53b86d839fbe40a8be68f11005bafd119751945a
Prachi I <prachi.indurkar007@gmail.com>
2023-04-08 08:41:31 +0530

    import hashrouter, mydashboard, myproduct, newproduct and myorder in adminapp file

---
+++ src/admin/adminapp.js
@@ -0,0 +1,5 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Mydashboard from "./dashboard";
+import Myorder from "./order";
+import Myproduct from "./product";
+import Newproduct from "./newproduct";
