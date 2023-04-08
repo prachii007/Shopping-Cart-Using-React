0e47122ef171ba5c2f95e06da332474374ff0dbc
Prachi I <prachi.indurkar007@gmail.com>
2023-04-08 08:31:12 +0530

    add correct links to publicheader

--- src/admin/adminheader.js
+++ src/admin/adminheader.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Adminheader = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-dark p-3 sticky-top">
+        <nav className="navbar navbar-expand-lg bg-info p-3 sticky-top">
             <div className="container-fluid">
                 <a className="navbar-brand text-white" href="#"><i className="fa fa-shopping-bag fa-lg"></i> React Shopping App</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -11,13 +11,24 @@ const Adminheader = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item ps-5">
-                            <Link className="nav-link text-white" aria-current="page" to="#"><i className="fa fa-home"></i> Shopping</Link>
+                            <Link className="nav-link text-white" aria-current="page" to=""><i className="fa fa-cogs"></i>
+                                Dashboard</Link>
                         </li>
                         <li className="nav-item ps-5">
-                            <Link className="nav-link text-white" to="/cart"><i className="fa fa-shopping-cart"></i>My Cart</Link>
+                            <Link className="nav-link text-white" to="/order"><i className="fa fa-suitcase-cart"></i>
+                                My Order</Link>
                         </li>
                         <li className="nav-item ps-5">
-                            <Link className="nav-link text-white" to="/login"><i className="fa fa-lock"></i> Login</Link>
+                            <Link className="nav-link text-white" to="/product"><i className="fa fa-table"></i>
+                                All Products</Link>
+                        </li>
+                        <li className="nav-item ps-5">
+                            <Link className="nav-link text-white" to="/addproduct"><i className="fa fa-plus"></i>
+                                Add Products</Link>
+                        </li>
+                        <li className="nav-item ps-5">
+                            <a className="nav-link text-danger"><i className="fa fa-power-off"></i>
+                                Welcome Admin</a>
                         </li>
                     </ul>
                 </div>
