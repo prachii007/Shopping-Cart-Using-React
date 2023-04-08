e6b34ad14651d3c99b22f5bdcc45da46fb3f3ab9
Prachi I <prachi.indurkar007@gmail.com>
2023-04-08 08:44:17 +0530

    import adminheader and route links for components already imported in adminapp file

--- src/admin/adminapp.js
+++ src/admin/adminapp.js
@@ -3,8 +3,18 @@ import Mydashboard from "./dashboard";
 import Myorder from "./order";
 import Myproduct from "./product";
 import Newproduct from "./newproduct";
+import Adminheader from "./adminheader";
 
 const AdminApp =()=>{
-    
+    <HashRouter>
+    <Publicheader />
+    <Routes>
+        <Route exact path="/" element={<Mydashboard />} />
+        <Route exact path="/login" element={<Myorder />} />
+        <Route exact path="/cart" element={<Myproduct />} />
+        <Route exact path="/cart" element={<Newproduct />} />
+
+    </Routes>
+</HashRouter >
 }
 export default AdminApp;
