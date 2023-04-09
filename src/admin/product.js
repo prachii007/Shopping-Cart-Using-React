70f2a263dc7e7c5617f9f67b8e0fb9215e76e182
Prachi I <prachi.indurkar007@gmail.com>
2023-04-09 09:17:32 +0530

    import usestate, useffect, toastcontainer

--- src/admin/product.js
+++ src/admin/product.js
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { useState, useEffect } from "react";
 const Myproduct = () => {
     return (
         <h1>Product List</h1>
