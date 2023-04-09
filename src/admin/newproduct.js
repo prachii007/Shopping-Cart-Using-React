13db48df880d6ec82d372f51a19530b1830977f3
Prachi I <prachi.indurkar007@gmail.com>
2023-04-09 08:55:12 +0530

    define save function

--- src/admin/newproduct.js
+++ src/admin/newproduct.js
@@ -5,7 +5,11 @@ const Newproduct = () => {
     let [pprice, pickPrice] = useState("");
     let [pphoto, pickPhoto] = useState("");
     let [pdetails, pickDetails] = useState("");
-    
+
+    const save = () => {
+        let sellerid = localStorage.getItem("sellerid");
+        alert(pname + pprice + pdetails + pphoto)
+    }
     return (
         <section className="container mt-4">
             <div className="row">
@@ -19,27 +23,32 @@ const Newproduct = () => {
                 <div className="col-lg-6">
                     <div className="mb-3">
                         <label>Enter Product Name</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control"
+                            value={pname} onChange={obj => pickName(obj.target.value)} />
                     </div>
                     <div className="mb-3">
                         <label>Enter Price/Unit</label>
-                        <input type="number" className="form-control" />
+                        <input type="number" className="form-control"
+                            value={pprice} onChange={obj => pickPrice(obj.target.value)} />
                     </div>
                     <div className="mb-3">
                         <label>Enter Photo URL</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control"
+                            value={pphoto} onChange={obj => pickPhoto(obj.target.value)} />
                     </div>
                     <div className="mb-3">
                         <label>Enter Product Details</label>
-                        <textarea type="text" className="form-control"></textarea>
-                    </div>
-                    <div className="text-center">
-                        <button className="btn btn-danger">Save Product</button>
-                    </div>
+                        <textarea type="text" className="form-control"
+                            value={pdetails} onChange={obj => pickDetails(obj.target.value)}>
+                    </textarea>
                 </div>
-                <div className="col-lg-3"></div>
+                <div className="text-center">
+                    <button onClick={save} className="btn btn-danger">Save Product</button>
+                </div>
             </div>
-        </section>
+            <div className="col-lg-3"></div>
+        </div>
+        </section >
     )
 
 }
