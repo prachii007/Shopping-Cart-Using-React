b8735cb50996aadf5814205db81920ca3e187da2
Prachi I <prachi.indurkar007@gmail.com>
2023-04-09 08:40:26 +0530

    add input boxes to enter product details in newproduct file

--- src/admin/newproduct.js
+++ src/admin/newproduct.js
@@ -1,6 +1,41 @@
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 const Newproduct = () => {
+    
     return (
-        <h1>New Product</h1>
+        <section className="container mt-4">
+            <div className="row">
+                <div className="col-lg-12 text-center mb-3">
+                    <h1 className="text-primary">Enter New Product Details</h1>
+                    <ToastContainer />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-lg-3"></div>
+                <div className="col-lg-6">
+                    <div className="mb-3">
+                        <label>Enter Product Name</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="mb-3">
+                        <label>Enter Price/Unit</label>
+                        <input type="number" className="form-control" />
+                    </div>
+                    <div className="mb-3">
+                        <label>Enter Photo URL</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="mb-3">
+                        <label>Enter Product Details</label>
+                        <textarea type="text" className="form-control"></textarea>
+                    </div>
+                    <div className="text-center">
+                        <button className="btn btn-danger">Save Product</button>
+                    </div>
+                </div>
+                <div className="col-lg-3"></div>
+            </div>
+        </section>
     )
 
 }
