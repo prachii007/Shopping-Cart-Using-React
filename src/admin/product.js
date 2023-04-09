03540f747734d41fd8964bc044f45b978de9b030
Prachi I <prachi.indurkar007@gmail.com>
2023-04-09 09:26:25 +0530

    define getproduct function and fetch data from seller's product list

--- src/admin/product.js
+++ src/admin/product.js
@@ -1,9 +1,22 @@
-import { useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
 import { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
 const Myproduct = () => {
+    let [allproduct, updateProduct] = useState([]);
+    const getProduct = () => {
+        let sellerid = localStorage.getItem("sellerid");
+        let url = "http://localhost:1234/product?seller=" + sellerid;
+        fetch(url)
+            .then(response => response.json())
+            .then(productArray => {
+                updateProduct(productArray);
+            })
+    }
+    useEffect(() => {
+        getProduct();
+
+    }, [1]);
     return (
-        <h1>Product List</h1>
+        <h1>Product List : {allproduct.length}</h1>
     )
 
 }
