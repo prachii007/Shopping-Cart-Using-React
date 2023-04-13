4cc180b82def5aa620de5ce7c687c652ee070317
Prachi I <prachi.indurkar007@gmail.com>
2023-04-13 16:30:09 +0530

    define variables using useState to pick up details entered with input types

--- src/user/signup.js
+++ src/user/signup.js
@@ -1,10 +1,17 @@
 import { useState, useEffect } from "react";
 const Signup = () => {
+    let [msg, updatemsg] = useState("Enter details to create an admin account");
+    let [fullname, pickFullname] = useState("");
+    let [username, pickUsername] = useState("");
+    let [pass, pickPassword] = useState("");
+    // const goSignup=()=>{
+
+    // }
     return (
         <section className="container mt-4">
             <div className="row justify-content-center">
                 <div className="col-lg-4">
-                    <p className="text-center text-danger">Enter details to create an admin account</p>
+                    <p className="text-center text-danger">{msg}</p>
                     <div className="card border-0 shadow-lg">
                         <div className="card-header bg-primary text-white">
                             <i className="fa fa-lock"></i> Signup
