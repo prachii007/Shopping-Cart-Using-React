6baf55bc961f0474b6da8d268cba28dea90de9cf
Prachi I <prachi.indurkar007@gmail.com>
2023-04-15 08:23:12 +0530

    send data from the order file in admin folder to json file

--- src/admin/order.js
+++ src/admin/order.js
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react";
 const Myorder = () => {
+    let [allorder, updateOrder] = useState([]);
+    const getOrder = () => {
+        let sellerid = localStorage.getItem("sellerid");
+        let url = "http://localhost:1234/order";
+        fetch(url)
+            .then(response => response.json())
+            .then(productArray => {
+                updateOrder(productArray.reverse())
+            })
+    }
+    useEffect(() => {
+
+    }, [1]);
     return (
         <h1>My Order</h1>
     )
