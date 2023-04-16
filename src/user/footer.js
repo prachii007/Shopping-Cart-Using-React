a7bccc0a0af2937e8365916024c75816aaf711b0
Prachi I <prachi.indurkar007@gmail.com>
2023-04-16 09:27:35 +0530

    add address and icons to footer

--- src/user/footer.js
+++ src/user/footer.js
@@ -1,6 +1,6 @@
 const Myfooter = () => {
     return (
-        <footer className="mt-4 bg-primary text-white p-5">
+        <footer className="mt-5 bg-primary text-white p-5">
             <div className="row">
                 <div className="col-lg-4">
                     <h4>About Us</h4>
@@ -12,6 +12,18 @@ const Myfooter = () => {
                         This is a footer. This is a footer.
                         This is a footer. This is a footer.
                     </p>
+                </div>
+                <div className="col-lg-4">
+                    <h4>Contact Us</h4>
+                    <p>+91-9876543210</p>
+                    <p>Email ID: hr.shoppers@gmail.com</p>
+                    <p>Marathalli, Bangalore 560037</p>
+                </div>
+                <div className="col-lg-4">
+                    <h4>Social Media</h4>
+                    <p><i className="fab fa-facebook fa-2x"></i> Facebook</p>
+                    <p><i className="fab fa-twitter fa-2x"></i> Twitter</p>
+                    <p><i className="fab fa-instagram fa-2x"></i> Instagram</p>
                 </div>
             </div>
         </footer>
