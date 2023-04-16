1da58e8cab8d6581f39e54655169e9ef51974a4f
Prachi I <prachi.indurkar007@gmail.com>
2023-04-16 09:18:11 +0530

    define myfooter function

---
+++ src/user/footer.js
@@ -0,0 +1,20 @@
+const Myfooter = () => {
+    return (
+        <footer className="mt-4 bg-primary text-white p-5">
+            <div className="row">
+                <div className="col-lg-4">
+                    <h4>About Us</h4>
+                    <p>
+                        This is a footer. This is a footer.
+                        This is a footer. This is a footer.
+                        This is a footer. This is a footer.
+                        This is a footer. This is a footer.
+                        This is a footer. This is a footer.
+                        This is a footer. This is a footer.
+                    </p>
+                </div>
+            </div>
+        </footer>
+    )
+}
+export default Myfooter;
