918f358f891a16ecc844249777532c39a5ee7385
Prachi I <prachi.indurkar007@gmail.com>
2023-05-03 12:38:34 +0530

    make all input text fields compulsory for submission

--- src/admin/newproduct.js
+++ src/admin/newproduct.js
@@ -8,31 +8,38 @@ const Newproduct = () => {
     let [pphotoType, pickPhotoType] = useState("");
 
     const save = () => {
-        let sellerid = localStorage.getItem("sellerid");
-        let url = "https://shopping-api-ypz4.onrender.com/product";
-        let pinfo = {
-            "name": pname,
-            "price": pprice,
-            "photo": {
-                [pphotoType]: pphoto
-            },
-            "details": pdetails,
-            "seller": sellerid
-        };
-        let postoption = {
-            headers: { 'Content-Type': 'application/json' },
-            method: "POST",
-            body: JSON.stringify(pinfo)
+        let formstatus = true;
+        if (pname === "" || pprice === "" | pphoto === "" || pdetails === "") {
+            formstatus = false;
+            alert("All fields are compulsory")
         }
-        fetch(url, postoption)
-            .then(response => response.json())
-            .then(serverRes => {
-                toast(pname + " Saved Successfully")
-                pickName("");
-                pickPrice("");
-                pickDetails("");
-                pickPhoto("");
-            })
+        if (formstatus === true) {
+            let sellerid = localStorage.getItem("sellerid");
+            let url = "https://shopping-api-ypz4.onrender.com/product";
+            let pinfo = {
+                "name": pname,
+                "price": pprice,
+                "photo": {
+                    [pphotoType]: pphoto
+                },
+                "details": pdetails,
+                "seller": sellerid
+            };
+            let postoption = {
+                headers: { 'Content-Type': 'application/json' },
+                method: "POST",
+                body: JSON.stringify(pinfo)
+            }
+            fetch(url, postoption)
+                .then(response => response.json())
+                .then(serverRes => {
+                    toast(pname + " Saved Successfully")
+                    pickName("");
+                    pickPrice("");
+                    pickDetails("");
+                    pickPhoto("");
+                })
+        }
     }
     return (
         <section className="container mt-4">
@@ -46,32 +53,33 @@ const Newproduct = () => {
                 <div className="col-lg-3"></div>
                 <div className="col-lg-6">
                     <div className="mb-3">
-                        <label>Enter Product Name</label>
+                        <label>Enter Product Name <span className="text-danger">*</span></label>
                         <input type="text" className="form-control" maxLength={16}
                             value={pname} onChange={obj => pickName(obj.target.value)} />
                     </div>
                     <div className="mb-3">
-                        <label>Enter Price/Unit</label>
+                        <label>Enter Price/Unit <span className="text-danger">*</span></label>
                         <input type="number" className="form-control"
                             value={pprice} onChange={obj => pickPrice(obj.target.value)} />
                     </div>
                     <div className="mb-3">
-                    <label>
-                     Where is your photo located?
-                    </label>
-                        <select className="form-select" onChange={obj=>pickPhotoType(obj.target.value)}>
+                        <label>
+                            Where is your photo located? <span className="text-danger">*</span> 
+                        </label>
+                        <select className="form-select" onChange={obj => pickPhotoType(obj.target.value)}>
+                            <option>--Choose Any One--</option>
                             <option value="local">Locally On This Website</option>
                             <option value="absolute">Globally On The Internet</option>
                         </select>
                     </div>
 
                     <div className="mb-3">
-                        <label>Enter Photo URL</label>
+                        <label>Enter Photo URL <span className="text-danger">*</span> <span className="fw-italic text-success">For local images write 1.jpg, 2.jpg, ---, 12.jpg</span></label>
                         <input type="text" className="form-control"
                             value={pphoto} onChange={obj => pickPhoto(obj.target.value)} />
                     </div>
                     <div className="mb-3">
-                        <label>Enter Product Details</label>
+                        <label>Enter Product Details <span className="text-danger">*</span></label>
                         <textarea type="text" className="form-control" maxLength={30}
                             value={pdetails} onChange={obj => pickDetails(obj.target.value)}>
                         </textarea>
